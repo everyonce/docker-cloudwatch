809d9cc089cad3a7658803e3d237d4a6439e8787
everyonce <daniel@everyonce.com>
2018-12-22 05:45:55 +0000

    fix for periods in container names

--- send_stats.sh
+++ send_stats.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-docker stats --all --format "{{.Name}} {{.CPUPerc}} {{.MemUsage}} {{.MemPerc}} {{.NetIO}} {{.BlockIO}} {{.PIDs}}" --no-stream \
+docker stats --format "{{.Name}} {{.CPUPerc}} {{.MemUsage}} {{.MemPerc}} {{.NetIO}} {{.BlockIO}} {{.PIDs}}" --no-stream \
  | while IFS= read -r line; do { \
 set -- $line; \
 containername="$1"; \
-line2=$(echo "$line" | sed 's/[^0-9\. ]*//g'); \
+line2=$(echo "$line" | sed 's/^\S*./name /g' | sed 's/[^0-9\. ]*//g'); \
 echo name: $containername; \
 set -- $line2; \
-aws cloudwatch put-metric-data --metric-name ContainerCPU --namespace DockerEngine --unit Percent --value $1 --dimensions name=$containername; \
-aws cloudwatch put-metric-data --metric-name ContainerMemory --namespace DockerEngine --unit Megabytes --value $2 --dimensions name=$containername; \
-aws cloudwatch put-metric-data --metric-name ContainerNetwork --namespace DockerEngine --unit Bytes/Second --value $5 --dimensions name=$containername; \
-aws cloudwatch put-metric-data --metric-name ContainerDisk --namespace DockerEngine --unit Bytes/Second --value $6 --dimensions name=$containername; \
+aws cloudwatch put-metric-data --metric-name ContainerCPU --namespace DockerEngine --unit Percent --value "$1" --dimensions name=$containername; \
+aws cloudwatch put-metric-data --metric-name ContainerMemory --namespace DockerEngine --unit Megabytes --value "$2" --dimensions name=$containername; \
+aws cloudwatch put-metric-data --metric-name ContainerNetwork --namespace DockerEngine --unit Bytes/Second --value "$5" --dimensions name=$containername; \
+aws cloudwatch put-metric-data --metric-name ContainerDisk --namespace DockerEngine --unit Bytes/Second --value "$6" --dimensions name=$containername; \
 } done
