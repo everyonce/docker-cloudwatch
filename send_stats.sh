482b114175ec28cf7cd721f5d038dd1baec28c35
everyonce <daniel@everyonce.com>
2018-12-22 06:03:14 +0000

    need full path for aws command to run in cron

--- send_stats.sh
+++ send_stats.sh
@@ -6,8 +6,8 @@ containername="$1"; \
 line2=$(echo "$line" | sed 's/^\S*./name /g' | sed 's/[^0-9\. ]*//g'); \
 echo name: $containername; \
 set -- $line2; \
-aws cloudwatch put-metric-data --metric-name ContainerCPU --namespace DockerEngine --unit Percent --value "$1" --dimensions name=$containername; \
-aws cloudwatch put-metric-data --metric-name ContainerMemory --namespace DockerEngine --unit Megabytes --value "$2" --dimensions name=$containername; \
-aws cloudwatch put-metric-data --metric-name ContainerNetwork --namespace DockerEngine --unit Bytes/Second --value "$5" --dimensions name=$containername; \
-aws cloudwatch put-metric-data --metric-name ContainerDisk --namespace DockerEngine --unit Bytes/Second --value "$6" --dimensions name=$containername; \
+/home/ubuntu/.local/bin/aws cloudwatch put-metric-data --metric-name ContainerCPU --namespace DockerEngine --unit Percent --value "$1" --dimensions name=$containername; \
+/home/ubuntu/.local/bin/aws cloudwatch put-metric-data --metric-name ContainerMemory --namespace DockerEngine --unit Megabytes --value "$2" --dimensions name=$containername; \
+/home/ubuntu/.local/bin/aws cloudwatch put-metric-data --metric-name ContainerNetwork --namespace DockerEngine --unit Bytes/Second --value "$5" --dimensions name=$containername; \
+/home/ubuntu/.local/bin/aws cloudwatch put-metric-data --metric-name ContainerDisk --namespace DockerEngine --unit Bytes/Second --value "$6" --dimensions name=$containername; \
 } done
